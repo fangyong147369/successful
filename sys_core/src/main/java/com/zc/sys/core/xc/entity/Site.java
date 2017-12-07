235c10371af6d6fea8667e65c7e8f5a532abe468
tom <123456>
2017-12-07 11:05:37 +0800

    javabean

--- sys_core/src/main/java/com/zc/sys/core/xc/entity/Site.java
+++ sys_core/src/main/java/com/zc/sys/core/xc/entity/Site.java
@@ -15,5 +15,75 @@ import com.zc.sys.core.common.constant.BaseConstant;
 public class Site extends LongPKEntity {
 	/** 序列号 **/
 	private static final long serialVersionUID = 1L;
+	/** 名称 **/
+	private String name;
+	/** 标识 **/
+	private String nid;
+	/** 父id **/
+	private Long pid;
+	/** 状态，-1：禁用，1：启用 **/
+	private int state;
+	/** 类型  **/
+	private int type;
+	/** 跳转链接**/
+	private String url;
+	/** 排序 **/
+	private int sort;
+	/** 简介 **/
+	private String introduction;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getNid() {
+		return nid;
+	}
+	public void setNid(String nid) {
+		this.nid = nid;
+	}
+	public Long getPid() {
+		return pid;
+	}
+	public void setPid(Long pid) {
+		this.pid = pid;
+	}
+	public int getState() {
+		return state;
+	}
+	public void setState(int state) {
+		this.state = state;
+	}
+	public int getType() {
+		return type;
+	}
+	public void setType(int type) {
+		this.type = type;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public int getSort() {
+		return sort;
+	}
+	public void setSort(int sort) {
+		this.sort = sort;
+	}
+	public String getIntroduction() {
+		return introduction;
+	}
+	public void setIntroduction(String introduction) {
+		this.introduction = introduction;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	
 
 }
