7bcff9c37beabe682df81ebc56c26663a3114b71
tom <123456>
2017-12-15 09:55:39 +0800

    1

--- sys_api_zc/src/main/java/com/zc/sys/api/zc/Application.java
+++ sys_api_zc/src/main/java/com/zc/sys/api/zc/Application.java
@@ -20,7 +20,6 @@ public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
- 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(new Class[]{Application.class});
